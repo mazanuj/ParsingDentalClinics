6f0ccfa58079473b6dbd5e5b0dff9dc2771c6235
mazanuj <mazanuj@gmail.com>
2014-10-10 01:35:31 +0300

    Edit SiteDataFactory

---
+++ ParsingDentalClinics/Sites/AllBizData.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+using ParsingDentalClinics.Config;
+using ParsingDentalClinics.Interfaces;
+
+namespace ParsingDentalClinics.Sites
+{
+    internal class AllBizData : ISiteData
+    {
+        public IEnumerable<InfoHolder> GetInfo()
+        {
+            var holdersList = new List<InfoHolder>();
+
+            var wc = new WebClient();
+            wc.Headers.Add(HttpRequestHeader.Cookie, "pager_per_page=48");
+            wc.Headers.Add(HttpRequestHeader.Cookie, "test=ok");
+            var html = wc.DownloadString("http://baku.all.biz/stomatologicheskie-uslugi-bsr1675");
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+            var ads =
+                doc.DocumentNode.Descendants("div")
+                    .Where(x => x.Attributes.Contains("class") && x.Attributes["class"].Value == "b-product--list-item ");
+
+            foreach (var url in ads.Select(ad => ad.Descendants("a")
+                .First(
+                    x =>
+                        x.Attributes.Contains("href") &&
+                        Regex.IsMatch(x.Attributes["href"].Value, @"http://\d*.az.all.biz/contacts"))
+                .Attributes["href"].Value + "?show=phones"))
+            {
+                doc = new HtmlWeb().Load(url);
+                var contacts = doc.DocumentNode.Descendants("div")
+                    .First(x => x.Attributes.Contains("id") && x.Attributes["id"].Value == "contacts");
+
+                var infoHolder = new InfoHolder
+                {
+                    Site = SiteEnum.AllBiz,
+                    Country = CountryEnum.Azerbaijan,
+                    ClinicName = RegExpression(
+                        contacts.Descendants("h1")
+                            .First(x => x.Attributes.Contains("itemprop") && x.Attributes["itemprop"].Value == "name")
+                            .InnerText),
+                    Phone =
+                        RegExpression(string.Join("; ",
+                            contacts.Descendants("span")
+                                .Where(
+                                    x =>
+                                        x.Attributes.Contains("itemprop") &&
+                                        x.Attributes["itemprop"].Value == "telephone")
+                                .Select(y => y.InnerText)
+                            )),
+                    Address = RegExpression(string.Format("{0}, {1}, {2}",
+                        contacts.Descendants("span")
+                            .First(x => x.Attributes.Contains("class") && x.Attributes["class"].Value == "country-name")
+                            .InnerText,
+                        contacts.Descendants("span")
+                            .First(x => x.Attributes.Contains("class") && x.Attributes["class"].Value == "locality")
+                            .InnerText,
+                        //contacts.Descendants("span")
+                        //    .First(
+                        //        x => x.Attributes.Contains("itemprop") && x.Attributes["itemprop"].Value == "postalCode")
+                        //    .InnerText,
+                        contacts.Descendants("span")
+                            .First(
+                                x =>
+                                    x.Attributes.Contains("itemprop") &&
+                                    x.Attributes["itemprop"].Value == "streetAddress")
+                            .InnerText))
+                };
+                holdersList.Add(infoHolder);
+            }
+            return holdersList;
+        }
+
+        public string RegExpression(string textInput)
+        {
+            var text = textInput.Replace("&quot;", string.Empty)
+                .Replace("\n", string.Empty)
+                .Replace("&nbsp;", string.Empty);
+            text = Regex.Replace(text, @"\s+", " ");
+            return Regex.Replace(text, @"(^\s+)|(\s+$)", string.Empty);
+        }
+    }
+}
---
+++ ParsingDentalClinics/Sites/BakuSearchData.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+using ParsingDentalClinics.Config;
+using ParsingDentalClinics.Interfaces;
+
+namespace ParsingDentalClinics.Sites
+{
+    internal class BakuSearchData : ISiteData
+    {
+        public IEnumerable<InfoHolder> GetInfo()
+        {
+            var holdersList = new List<InfoHolder>();
+            var htmlByte = new WebClient().DownloadData("http://www.bakusearch.info/russian/medical/stom_clinic.php");
+            var html = Encoding.GetEncoding("windows-1251").GetString(htmlByte);
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            var tables =
+                doc.DocumentNode.Descendants("table")
+                    .Where(
+                        x =>
+                            x.Attributes.Contains("width") && x.Attributes["width"].Value == "96%" &&
+                            !x.Attributes.Contains("height") && x.Attributes.Contains("cellpadding"));
+
+            foreach (var table in tables)
+            {
+                IEnumerable<string> td;
+                try
+                {
+                    td =
+                        table.Descendants("td")
+                            .First(
+                                x => x.Attributes.Contains("class") && x.Attributes["class"].Value == "black_verdana_11")
+                            .InnerText.Split('\n')
+                            .Where(y => !string.IsNullOrEmpty(y));
+                }
+                catch
+                {
+                    continue;
+                }
+
+                var infoHolder = new InfoHolder
+                {
+                    Site = SiteEnum.Bakusearch,
+                    Country = CountryEnum.Azerbaijan,
+                    ClinicName = table.Descendants("div")
+                        .First(x => x.Attributes.Contains("class") && x.Attributes["class"].Value == "blue_verdana_12")
+                        .InnerText
+                };
+                foreach (var value in td)
+                {
+                    if (Regex.IsMatch(value, @"(тел|факс|моб)"))
+                    {
+                        if (!string.IsNullOrEmpty(infoHolder.Phone))
+                            infoHolder.Phone += "; " + RegExpression(value);
+                        else infoHolder.Phone = RegExpression(value);
+                    }
+                    else if (value.Contains("e-mail"))
+                        infoHolder.Mail = RegExpression(value.Replace("e-mail:", ""));
+                    else if (!value.Contains("URL"))
+                        infoHolder.Address = RegExpression(value);
+                }
+                holdersList.Add(infoHolder);
+            }
+
+            return holdersList;
+        }
+
+        public string RegExpression(string textInput)
+        {
+            var text = textInput.Replace("&quot;", string.Empty)
+                .Replace("\n", string.Empty)
+                .Replace("&nbsp;", string.Empty);
+            text = Regex.Replace(text, @"\s+", " ");
+            return Regex.Replace(text, @"(^\s+)|(\s+$)", string.Empty);
+        }
+    }
+}
